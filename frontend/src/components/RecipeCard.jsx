9fc5075ba9ca16f5ae9bf28f139cd45f9f253a85
Erin Von Gertzen <ianvon@shaw.ca>
2024-04-14 21:01:13 -0600

    fixed issue with recipe card which was showing onclick when it shouldnt have

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const RecipeCard = ({ recipe, onClick }) => {
+const RecipeCard = ({ recipe }) => {
   return (
-    <div className="recipe-card" key={recipe.id} onClick={() => onClick(recipe)}>
+    <div className="recipe-card" key={recipe.id}>
       <img src={recipe.image} alt={recipe.title} />
       <div className="recipe-card-title"><h3>{recipe.title}</h3></div>
     </div>
