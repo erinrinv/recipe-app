f38fb2e3931c326a819c5b1b35c7346af5472ff1
evanb <labber@Evangelos>
2024-04-17 18:37:33 -0400

    final changes for user reg

---
+++ backend/db/users.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../db');
+const bcrypt = require('bcrypt');
+
+router.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  
+  try {
+    const newUser = await pool.query(
+      'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING user_id, username, email',
+      [username, email, hashedPassword]
+    );
+    res.status(201).json(newUser.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    if (user.rows.length > 0) {
+      const isValid = await bcrypt.compare(password, user.rows[0].password);
+      if (isValid) {
+        res.status(200).json({ message: "Login successful" });
+      } else {
+        res.status(401).json({ error: "Invalid Password" });
+      }
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
