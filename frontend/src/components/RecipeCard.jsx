6042f44e100e49fef47bf28fa18e73963a5bb0bf
Erin Von Gertzen <ianvon@shaw.ca>
2024-04-23 10:51:27 -0600

    fixed issue with missing code from recipe card component and linking to recipe page

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
 import Card from '@mui/material/Card';
+import { Link } from 'react-router-dom';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardActions from '@mui/material/CardActions';
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import InfoIcon from '@mui/icons-material/Info';
+
+
 
 function RecipeCard({ recipe, isFavorite, onToggleFavorite }) {
   return (
+    <div className="recipe-card" key={recipe.id}>
     <Card sx={{ maxWidth: 345 }}>
+      <Link to={`/${recipe.id}`} style={{ textDecoration: 'none' }}>
+        <CardHeader
+          action={
+            <IconButton aria-label="settings">
+              <InfoIcon />
+            </IconButton>
+          }
+          title={recipe.title}
+        />
+      </Link>
+
+    
+   
       <CardHeader
         title={recipe.title}
         // Other props
@@ -25,6 +43,7 @@ function RecipeCard({ recipe, isFavorite, onToggleFavorite }) {
         </IconButton>
       </CardActions>
     </Card>
+    </div>
   );
 }
 
