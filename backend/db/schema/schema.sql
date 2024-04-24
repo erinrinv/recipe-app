d1cdd034b1ea8cee15efade5dd511f560357e4bc
evanb <labber@Evangelos>
2024-04-24 17:49:41 -0400

    db changes

--- backend/db/schema/schema.sql
+++ backend/db/schema/schema.sql
@@ -1,257 +1,47 @@
---DROP TABLE recipes;
---DROP TABLE favorites;
---DROP TABLE users;
---DROP TABLE recipe_ingredients;
---DROP TABLE ingredients;
+DROP TABLE recipes;
+DROP TABLE favorites;
+DROP TABLE users;
+DROP TABLE recipe_ingredients;
+DROP TABLE ingredients;
 
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
 
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- Name: favorites; Type: TABLE; Schema: public; Owner: labber
---
-
-CREATE TABLE public.favorites (
-    favorite_id integer NOT NULL,
+CREATE TABLE favorites (
+    favorite_id serial NOT NULL,
     user_id integer NOT NULL,
     recipe_id integer NOT NULL,
     added_date timestamp with time zone DEFAULT CURRENT_TIMESTAMP
 );
 
+-- CREATE TABLE ingredients (
+--     ingredient_id serial NOT NULL,
+--     name character varying(255) NOT NULL,
+--     description text
+-- );
 
-ALTER TABLE public.favorites OWNER TO labber;
+-- CREATE TABLE recipe_ingredients (
+--     recipe_ingredients_id serial NOT NULL,
+--     recipe_id integer NOT NULL,
+--     ingredient_id integer NOT NULL,
+--     quantity character varying(50),
+--     measurements character varying(50)
+-- );
 
---
--- Name: favorites_favorite_id_seq; Type: SEQUENCE; Schema: public; Owner: labber
---
-
-CREATE SEQUENCE public.favorites_favorite_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.favorites_favorite_id_seq OWNER TO labber;
-
---
--- Name: favorites_favorite_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: labber
---
-
-ALTER SEQUENCE public.favorites_favorite_id_seq OWNED BY public.favorites.favorite_id;
+-- CREATE TABLE public.recipes (
+--     recipe_id integer NOT NULL,
+--     title character varying(255) NOT NULL,
+--     summary text NOT NULL,
+--     spoon_recipe_id integer NOT NULL
+-- );
 
 
---
--- Name: ingredients; Type: TABLE; Schema: public; Owner: labber
---
-
-CREATE TABLE public.ingredients (
-    ingredient_id integer NOT NULL,
-    name character varying(255) NOT NULL,
-    description text
-);
-
-
-ALTER TABLE public.ingredients OWNER TO labber;
-
---
--- Name: ingredients_ingredient_id_seq; Type: SEQUENCE; Schema: public; Owner: labber
---
-
-CREATE SEQUENCE public.ingredients_ingredient_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.ingredients_ingredient_id_seq OWNER TO labber;
-
---
--- Name: ingredients_ingredient_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: labber
---
-
-ALTER SEQUENCE public.ingredients_ingredient_id_seq OWNED BY public.ingredients.ingredient_id;
-
-
---
--- Name: recipe_ingredients; Type: TABLE; Schema: public; Owner: labber
---
-
-CREATE TABLE public.recipe_ingredients (
-    recipe_ingredients_id integer NOT NULL,
-    recipe_id integer NOT NULL,
-    ingredient_id integer NOT NULL,
-    quantity character varying(50),
-    measurements character varying(50)
-);
-
-
-ALTER TABLE public.recipe_ingredients OWNER TO labber;
-
---
--- Name: recipe_ingredients_recipe_ingredients_id_seq; Type: SEQUENCE; Schema: public; Owner: labber
---
-
-CREATE SEQUENCE public.recipe_ingredients_recipe_ingredients_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.recipe_ingredients_recipe_ingredients_id_seq OWNER TO labber;
-
---
--- Name: recipe_ingredients_recipe_ingredients_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: labber
---
-
-ALTER SEQUENCE public.recipe_ingredients_recipe_ingredients_id_seq OWNED BY public.recipe_ingredients.recipe_ingredients_id;
-
-
---
--- Name: recipes; Type: TABLE; Schema: public; Owner: labber
---
-
-CREATE TABLE public.recipes (
-    recipe_id integer NOT NULL,
-    title character varying(255) NOT NULL,
-    summary text NOT NULL,
-    spoon_recipe_id integer NOT NULL
-);
-
-
-ALTER TABLE public.recipes OWNER TO labber;
-
---
--- Name: recipes_recipe_id_seq; Type: SEQUENCE; Schema: public; Owner: labber
---
-
-CREATE SEQUENCE public.recipes_recipe_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.recipes_recipe_id_seq OWNER TO labber;
-
---
--- Name: recipes_recipe_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: labber
---
-
-ALTER SEQUENCE public.recipes_recipe_id_seq OWNED BY public.recipes.recipe_id;
-
-
---
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.users (
-    user_id integer NOT NULL,
+CREATE TABLE users (
+    user_id serial NOT NULL,
     username character varying(255) NOT NULL,
     email character varying(255) NOT NULL,
     password text NOT NULL
 );
 
-
-ALTER TABLE public.users OWNER TO postgres;
-
---
--- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.users_user_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.users_user_id_seq OWNER TO postgres;
-
---
--- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
-
-
---
--- Name: favorites favorite_id; Type: DEFAULT; Schema: public; Owner: labber
---
-
-ALTER TABLE ONLY public.favorites ALTER COLUMN favorite_id SET DEFAULT nextval('public.favorites_favorite_id_seq'::regclass);
-
-
---
--- Name: ingredients ingredient_id; Type: DEFAULT; Schema: public; Owner: labber
---
-
-ALTER TABLE ONLY public.ingredients ALTER COLUMN ingredient_id SET DEFAULT nextval('public.ingredients_ingredient_id_seq'::regclass);
-
-
---
--- Name: recipe_ingredients recipe_ingredients_id; Type: DEFAULT; Schema: public; Owner: labber
---
-
-ALTER TABLE ONLY public.recipe_ingredients ALTER COLUMN recipe_ingredients_id SET DEFAULT nextval('public.recipe_ingredients_recipe_ingredients_id_seq'::regclass);
-
-
---
--- Name: recipes recipe_id; Type: DEFAULT; Schema: public; Owner: labber
---
-
-ALTER TABLE ONLY public.recipes ALTER COLUMN recipe_id SET DEFAULT nextval('public.recipes_recipe_id_seq'::regclass);
-
-
---
--- Name: users user_id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
-
-
---
--- Name: favorites favorites_pkey; Type: CONSTRAINT; Schema: public; Owner: labber
---
 
 ALTER TABLE ONLY public.favorites
     ADD CONSTRAINT favorites_pkey PRIMARY KEY (favorite_id);
@@ -332,16 +122,12 @@ ALTER TABLE ONLY public.favorites
 -- Name: recipe_ingredients recipe_ingredients_ingredient_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: labber
 --
 
-ALTER TABLE ONLY public.recipe_ingredients
-    ADD CONSTRAINT recipe_ingredients_ingredient_id_fkey FOREIGN KEY (ingredient_id) REFERENCES public.ingredients(ingredient_id);
+-- ALTER TABLE ONLY public.recipe_ingredients
+--     ADD CONSTRAINT recipe_ingredients_ingredient_id_fkey FOREIGN KEY (ingredient_id) REFERENCES public.ingredients(ingredient_id);
 
 
---
--- Name: recipe_ingredients recipe_ingredients_recipe_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: labber
---
-
-ALTER TABLE ONLY public.recipe_ingredients
-    ADD CONSTRAINT recipe_ingredients_recipe_id_fkey FOREIGN KEY (recipe_id) REFERENCES public.recipes(recipe_id);
+-- ALTER TABLE ONLY public.recipe_ingredients
+--     ADD CONSTRAINT recipe_ingredients_recipe_id_fkey FOREIGN KEY (recipe_id) REFERENCES public.recipes(recipe_id);
 
 
 --
